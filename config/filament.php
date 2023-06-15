0e81eedc06d642cf9f67cc0a946dcbd7145cdbae
Lorenzo Vimini <lorenzo.vimini@gmail.com>
2023-06-15 13:23:46 +0200

    update

--- config/filament.php
+++ config/filament.php
@@ -140,7 +140,8 @@ return [
         'path' => app_path('Filament/Widgets'),
         'register' => [
             Widgets\AccountWidget::class,
-            Widgets\FilamentInfoWidget::class,
+            \Awcodes\Overlook\Overlook::class
+            //Widgets\FilamentInfoWidget::class,
         ],
     ],
 
